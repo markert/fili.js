18cf6e6412d5e265025152b310d074319238ea37
Florian Markert <florian@scienceflow.de>
2014-11-29 19:11:43 +0100

    added more filter types

--- src/calcCoeffs.js
+++ src/calcCoeffs.js
@@ -4,26 +4,58 @@
   'use strict';
 
   var CalcCoeffs = function () {
+    var preCalc = function (params, coeffs) {
+      var Q = params.Q,
+        Fc = params.Fc,
+        Fs = params.Fs;
+      var pre = {};
+      var w = 2 * Math.PI * Fc / Fs;
+      pre.alpha = Math.sin(w) / (2 * Q);
+      pre.cw = Math.cos(w);
+      pre.a0 = 1 + pre.alpha;
+      coeffs.a.push((-2 * pre.cw) / pre.a0);
+      coeffs.a.push((1 - pre.alpha) / pre.a0);
+      return pre;
+    };
+    var initCoeffs = function () {
+      var coeffs = {};
+      coeffs.z = [0, 0];
+      coeffs.a = [];
+      coeffs.b = [];
+      coeffs.k = 1;
+      return coeffs;
+    }
     var self = {
-      bq: function (params) {
-        var coeffs = {};
-        coeffs.z = [0, 0];
-        coeffs.a = [];
-        coeffs.b = [];
-        var Q = params.Q,
-          Fc = params.Fc,
-          Fs = params.Fs;
-        var w = 2 * Math.PI * Fc / Fs;
-        var alpha = Math.sin(w) / (2 * Q);
-        var cw = Math.cos(w);
-        var a0 = 1 + alpha;
-
-        coeffs.b.push((1 - cw) / (2 * a0));
+      lowpass: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push((1 - p.cw) / (2 * p.a0));
         coeffs.b.push(2 * coeffs.b[0]);
         coeffs.b.push(coeffs.b[0]);
-        coeffs.a.push((-2 * cw) / a0);
-        coeffs.a.push((1 - alpha) / a0);
-        coeffs.k = 1;
+        return coeffs;
+      },
+      highpass: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push((1 + p.cw) / (2 * p.a0));
+        coeffs.b.push(-2 * coeffs.b[0]);
+        coeffs.b.push(coeffs.b[0]);
+        return coeffs;
+      },
+      notch: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push(1);
+        coeffs.b.push(-2 * coeffs.b[0]);
+        coeffs.b.push(coeffs.b[0]);
+        return coeffs;
+      },
+      bandpass: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push(p.alpha * params.Q);
+        coeffs.b.push(0);
+        coeffs.b.push(coeffs.b[0]);
         return coeffs;
       }
     };
