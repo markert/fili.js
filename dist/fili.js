a96f813992c3faf088285b3d14cba442e95df1b4
Florian Markert <florian@scienceflow.de>
2014-11-29 23:44:43 +0100

    new version

--- dist/fili.js
+++ dist/fili.js
@@ -1,32 +1,126 @@
-/*! fili 0.0.1 29-11-2014 */
+/*! fili 0.0.2 29-11-2014 */
 /*! Author: Florian Markert */
 /*! License: MIT */
-/* global define */
+/* global CalcCoeffs, define */
+/*jslint bitwise: true */
+(function (window) {
+  'use strict';
+
+  var getCoeffs = new CalcCoeffs();
+  var table = {
+    bessel: {
+      q: [
+        [0.57735026919],
+        [0.805538281842, 0.521934581669],
+        [1.02331395383, 0.611194546878, 0.510317824749]
+      ],
+      f: [
+        [1.27201964951],
+        [1.60335751622, 1.43017155999],
+        [1.9047076123, 1.68916826762, 1.60391912877]
+      ]
+    },
+    butterworth: {
+      q: [
+        [0.7071067811865475],
+        [0.54, 1.31],
+        [0.52, 0.7071067811865475, 1.93]
+      ],
+      f: [
+        [1],
+        [1, 1],
+        [1, 1, 1]
+      ]
+    }
+  };
+
+  var CalcCascades = function () {
+    var cnt = 0;
+    var self = {
+      getCoefficients: function (params) {
+        var filter = [];
+        if (params.order > 3) {
+          params.order = 3;
+        }
+        for (cnt = 0; cnt < params.order; cnt++) {
+          filter.push(getCoeffs[params.behavior]({
+            Fs: params.Fs,
+            Fc: params.Fc * table[params.characteristic].f[params.order - 1][cnt],
+            Q: table[params.characteristic].q[params.order - 1][cnt]
+          }));
+        }
+        return filter;
+      }
+    };
+    return self;
+  };
+  if (typeof module === 'object' && module && typeof module.exports === 'object') {
+    module.exports = CalcCascades;
+  } else {
+    window.CalcCascades = CalcCascades;
+    if (typeof define === 'function' && define.amd) {
+      define(CalcCascades);
+    }
+  }
+})(window);
+;/* global define */
 /*jslint bitwise: true */
 (function (window) {
   'use strict';
 
   var CalcCoeffs = function () {
+    var preCalc = function (params, coeffs) {
+      var Q = params.Q,
+        Fc = params.Fc,
+        Fs = params.Fs;
+      var pre = {};
+      var w = 2 * Math.PI * Fc / Fs;
+      pre.alpha = Math.sin(w) / (2 * Q);
+      pre.cw = Math.cos(w);
+      pre.a0 = 1 + pre.alpha;
+      coeffs.a.push((-2 * pre.cw) / pre.a0);
+      coeffs.a.push((1 - pre.alpha) / pre.a0);
+      return pre;
+    };
+    var initCoeffs = function () {
+      var coeffs = {};
+      coeffs.z = [0, 0];
+      coeffs.a = [];
+      coeffs.b = [];
+      coeffs.k = 1;
+      return coeffs;
+    };
     var self = {
-      bq: function (params) {
-        var coeffs = {};
-        coeffs.z = [0, 0];
-        coeffs.a = [];
-        coeffs.b = [];
-        var Q = params.Q,
-          Fc = params.Fc,
-          Fs = params.Fs;
-        var w = 2 * Math.PI * Fc / Fs;
-        var alpha = Math.sin(w) / (2 * Q);
-        var cw = Math.cos(w);
-        var a0 = 1 + alpha;
-
-        coeffs.b.push((1 - cw) / (2 * a0));
+      lowpass: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push((1 - p.cw) / (2 * p.a0));
         coeffs.b.push(2 * coeffs.b[0]);
         coeffs.b.push(coeffs.b[0]);
-        coeffs.a.push((-2 * cw) / a0);
-        coeffs.a.push((1 - alpha) / a0);
-        coeffs.k = 1;
+        return coeffs;
+      },
+      highpass: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push((1 + p.cw) / (2 * p.a0));
+        coeffs.b.push(-2 * coeffs.b[0]);
+        coeffs.b.push(coeffs.b[0]);
+        return coeffs;
+      },
+      notch: function (params) {
+        var coeffs = initCoeffs();
+        preCalc(params, coeffs);
+        coeffs.b.push(1);
+        coeffs.b.push(-2 * coeffs.b[0]);
+        coeffs.b.push(coeffs.b[0]);
+        return coeffs;
+      },
+      bandpass: function (params) {
+        var coeffs = initCoeffs();
+        var p = preCalc(params, coeffs);
+        coeffs.b.push(p.alpha * params.Q);
+        coeffs.b.push(0);
+        coeffs.b.push(coeffs.b[0]);
         return coeffs;
       }
     };
