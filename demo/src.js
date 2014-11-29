817650ede026916cbf7dbfb2e0d55f77b8ce776f
Florian Markert <florian@scienceflow.de>
2014-11-29 23:43:44 +0100

    updated

--- demo/src.js
+++ demo/src.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  /* global streamchief, signalviews */
+  /* global CalcCascades, Filter */
   'use strict';
 
   var input = document.getElementById('f_val');
@@ -7,50 +7,27 @@ $(document).ready(function () {
   var output = document.getElementById('f_res');
   var c = document.getElementById('f_coeffs');
 
-  var coeffsCalculator = new CalcCoeffs();
-  // Bessel
-  //1.02331395383  0.611194546878 0.510317824749
-  //1.9047076123    1.68916826762   1.60391912877 
-  var coeffs_Bessel = [];
-  coeffs_Bessel.push(coeffsCalculator.lowpass({
-    Q: 1.02331395383,
-    Fs: 96000,
-    Fc: 1.9047076123 * 9600
-  }));
-  coeffs_Bessel.push(coeffsCalculator.lowpass({
-    Q: 0.611194546878,
-    Fs: 96000,
-    Fc: 1.68916826762 * 9600
-  }));
-  coeffs_Bessel.push(coeffsCalculator.lowpass({
-    Q: 0.510317824749,
-    Fs: 96000,
-    Fc: 1.60391912877 * 9600
-  }));
+  var filterCalculator = new CalcCascades();
 
-  // Buuterworth
-  //0.541  0.707 1.93
-  //1    1   1
-  var coeffs_Butterworth = [];
-  coeffs_Butterworth.push(coeffsCalculator.lowpass({
-    Q: 0.541,
+  var coeffsBessel = filterCalculator.getCoefficients({
+    order: 3,
+    characteristic: 'bessel',
+    behavior: 'lowpass',
     Fs: 96000,
     Fc: 9600
-  }));
-  coeffs_Butterworth.push(coeffsCalculator.lowpass({
-    Q: 0.707,
-    Fs: 96000,
-    Fc: 9600
-  }));
-  coeffs_Butterworth.push(coeffsCalculator.lowpass({
-    Q: 1.93,
-    Fs: 96000,
-    Fc: 9600
-  }));
+  });
 
-  c.innerHTML = 'Bessel: ' + JSON.stringify(coeffs_Bessel) + '<br><br>' + 'Butterworth: ' + JSON.stringify(coeffs_Butterworth) + '<br><br>';
-  var filterBessel = new Filter(coeffs_Bessel);
-  var filterButterworth = new Filter(coeffs_Butterworth);
+  var coeffsButterworth = filterCalculator.getCoefficients({
+    order: 3,
+    characteristic: 'butterworth',
+    behavior: 'lowpass',
+    Fs: 96000,
+    Fc: 9600
+  });
+
+  c.innerHTML = 'Bessel: ' + JSON.stringify(coeffsBessel) + '<br><br>' + 'Butterworth: ' + JSON.stringify(coeffsButterworth) + '<br><br>';
+  var filterBessel = new Filter(coeffsBessel);
+  var filterButterworth = new Filter(coeffsButterworth);
   var outBessel = [];
   var outButterworth = [];
   var ostr = '';
