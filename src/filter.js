a9e644239089d1b0870a041576f1e783c985864f
Florian Markert <florian@scienceflow.de>
2014-11-30 17:26:46 +0100

    started to implement phase/magnitude simulation

--- src/filter.js
+++ src/filter.js
@@ -1,4 +1,4 @@
-/* global define */
+/* global define, Complex */
 /*jslint bitwise: true */
 (function (window) {
   'use strict';
@@ -7,6 +7,7 @@
   // stage structure e.g. {k:1, a:[1.1, -1.2], b:[0.3, -1.2, -0.4], z:[0, 0]}
   var Filter = function (filter) {
     var f = filter;
+    var complex = new Complex();
     var runStage = function (s, input) {
       var temp = input * s.k - s.a[0] * s.z[0] - s.a[1] * s.z[1];
       var out = s.b[0] * temp + s.b[1] * s.z[0] + s.b[2] * s.z[1];
@@ -33,12 +34,49 @@
       return out;
     };
 
+    var biquadResponse = function (params, s) {
+      var Fs = params.Fs,
+        Fr = params.Fr;
+      var theta = 2 * Math.PI * Fs / Fr;
+      var sin = Math.sin(theta);
+      var sin2 = sin * sin;
+      var x = (1 + sin2) / ((1 - sin2) - 4 * sin2);
+      var y = 2 * sin / ((1 - sin2) - 4 * sin2);
+      var p = {
+        re: s.b[0] + s.b[1] / (1 + sin2) + s.b[2] * x,
+        im: s.b[1] * sin / (1 - sin2) + s.b[2] * y
+      };
+      var q = {
+        re: 1 + s.a[0] / (1 + sin2) + s.a[1] * x,
+        im: s.a[0] * sin / (1 - sin2) + s.a[1] * y
+      };
+      var h = complex.div(p, q);
+      var res = {
+        magnitude: complex.magnitude(h),
+        phase: complex.phase(h)
+      };
+      return res;
+    };
+
     var self = {
       singleStep: function (input) {
         return runFilter(input);
       },
       multiStep: function (input) {
         return runMultiFilter(input);
+      },
+      response: function (params) {
+        var cnt = 0;
+        var res = {
+          magnitude: 1,
+          phase: 1
+        };
+        for (cnt = 0; cnt < f.length; cnt++) {
+          var r = biquadResponse(params, f[cnt]);
+          res.magnitude *= r.magnitude;
+          res.phase *= r.phase;
+        }
+        return res;
       }
     };
     return self;
