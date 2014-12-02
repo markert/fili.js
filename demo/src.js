7cd1f3259f9f4214bced4f422801989874bd15ff
Florian Markert <florian@scienceflow.de>
2014-12-02 16:47:05 +0100

    updated demo

--- demo/src.js
+++ demo/src.js
@@ -1,49 +1,134 @@
 $(document).ready(function () {
-  /* global CalcCascades, Filter */
+  /* global CalcCascades, FirFilter, CalcCascades, FirCoeffs, IirFilter */
   'use strict';
 
-  var input = document.getElementById('f_val');
+  var fs = document.getElementById('fs_val');
+  var fc = document.getElementById('fc_val');
+  var io = document.getElementById('iir_val');
+  var fo = document.getElementById('fir_val');
+  var buir = document.getElementById('buir_val');
+  var beir = document.getElementById('beir_val');
   var sim = document.getElementById('f_sim');
-  var output = document.getElementById('f_res');
-  var c = document.getElementById('f_coeffs');
 
-  var filterCalculator = new CalcCascades();
+  fs.value = 1000;
+  fc.value = 100;
+  io.value = 3;
+  fo.value = 100;
+  beir.value = 20;
+  buir.value = 40;
 
-  var coeffsBessel = filterCalculator.getCoefficients({
-    order: 3,
-    characteristic: 'bessel',
-    behavior: 'lowpass',
-    Fs: 96000,
-    Fc: 9600
-  });
-
-  var coeffsButterworth = filterCalculator.getCoefficients({
-    order: 3,
-    characteristic: 'butterworth',
-    behavior: 'lowpass',
-    Fs: 96000,
-    Fc: 9600
-  });
-
-  c.innerHTML = 'Bessel: ' + JSON.stringify(coeffsBessel) + '<br><br>' + 'Butterworth: ' + JSON.stringify(coeffsButterworth) + '<br><br>';
-  var filterBessel = new Filter(coeffsBessel);
-  var filterButterworth = new Filter(coeffsButterworth);
-  var outBessel = [];
-  var outButterworth = [];
-  var ostr = '';
-  var cnt = 0;
-  var x = filterBessel.response({
-    Fs: 96000,
-    Fr: 600
-  });
-  console.log(x);
   sim.onclick = function () {
-    var val = input.value;
-    outBessel.push(filterBessel.singleStep(val));
-    outButterworth.push(filterButterworth.singleStep(val));
-    ostr += cnt + ':' + outBessel[outBessel.length - 1] + ' ' + outButterworth[outButterworth.length - 1] + '<br>';
-    cnt++;
-    output.innerHTML = ostr;
+    var iirCalculator = new CalcCascades();
+    var firCalculator = new FirCoeffs();
+
+    var coeffsBessel = iirCalculator.lowpass({
+      order: io.value,
+      characteristic: 'bessel',
+      Fs: fs.value,
+      Fc: fc.value
+    });
+
+    var coeffsButterworth = iirCalculator.lowpass({
+      order: io.value,
+      characteristic: 'butterworth',
+      Fs: fs.value,
+      Fc: fc.value
+    });
+
+    var coeffsFir = firCalculator.lowpass({
+      order: fo.value,
+      Fs: fs.value,
+      Fc: fc.value
+    });
+
+    var filterBessel = new IirFilter(coeffsBessel);
+    var filterButterworth = new IirFilter(coeffsButterworth);
+    var filterFir = new FirFilter(coeffsFir);
+    var cnt = 0;
+    var iirBeRe = filterBessel.response(480);
+    var iirBeReMag = [];
+    var fss = fs.value;
+    for (cnt = 0; cnt < iirBeRe.length; cnt++) {
+      iirBeReMag.push([fss / 2 * cnt / iirBeRe.length, iirBeRe[cnt].magnitude]);
+    }
+    $.plot($('#iirmag'), [{
+      data: iirBeReMag,
+      color: '#FF0000'
+    }]);
+    var iirBeReGrp = [];
+    for (cnt = 0; cnt < iirBeRe.length; cnt++) {
+      iirBeReGrp.push([fss / 2 * cnt / iirBeRe.length, iirBeRe[cnt].groupDelay]);
+    }
+    $.plot($('#iirgroup'), [{
+      data: iirBeReGrp,
+      color: '#FF0000'
+    }]);
+    var iirBeReSr = filterBessel.impulseResponse(beir.value);
+    var iirBeReImp = [];
+    for (cnt = 0; cnt < iirBeReSr.out.length; cnt++) {
+      iirBeReImp.push([cnt, iirBeReSr.out[cnt]]);
+    }
+    $.plot($('#iirimp'), [{
+      data: iirBeReImp,
+      color: '#FF0000'
+    }]);
+
+    var iirBuRe = filterButterworth.response(480);
+    var iirBuReMag = [];
+    for (cnt = 0; cnt < iirBuRe.length; cnt++) {
+      iirBuReMag.push([fss / 2 * cnt / iirBuRe.length, iirBuRe[cnt].magnitude]);
+    }
+    $.plot($('#iirbmag'), [{
+      data: iirBuReMag,
+      color: '#00FF00'
+    }]);
+    var iirBuReGrp = [];
+    for (cnt = 0; cnt < iirBuRe.length; cnt++) {
+      iirBuReGrp.push([fss / 2 * cnt / iirBuRe.length, iirBuRe[cnt].groupDelay]);
+    }
+    $.plot($('#iirbgroup'), [{
+      data: iirBuReGrp,
+      color: '#00FF00'
+    }]);
+    var iirBuReSr = filterButterworth.impulseResponse(buir.value);
+    var iirBuReImp = [];
+    for (cnt = 0; cnt < iirBuReSr.out.length; cnt++) {
+      iirBuReImp.push([cnt, iirBuReSr.out[cnt]]);
+    }
+    $.plot($('#iirbimp'), [{
+      data: iirBuReImp,
+      color: '#00FF00'
+    }]);
+
+    var firRe = filterFir.response();
+
+    var firReMag = [];
+    for (cnt = 0; cnt < firRe.length; cnt++) {
+      firReMag.push([fss / 2 * cnt / firRe.length, firRe[cnt].dBmagnitude]);
+    }
+    $.plot($('#firmag'), [{
+      data: firReMag,
+      color: '#0000FF'
+    }]);
+    var firReGrp = [];
+    for (cnt = 0; cnt < firRe.length; cnt++) {
+      firReGrp.push([fss / 2 * cnt / firRe.length, firRe[cnt].groupDelay]);
+    }
+    $.plot($('#firgroup'), [{
+      data: firReGrp,
+      color: '#0000FF'
+    }]);
+
+    var firReImp = [];
+    for (cnt = 0; cnt < coeffsFir.length; cnt++) {
+      firReImp.push([cnt, coeffsFir[cnt]]);
+    }
+    $.plot($('#firimp'), [{
+      data: firReImp,
+      color: '#0000FF'
+    }]);
   };
+
+  sim.click();
 
 });
