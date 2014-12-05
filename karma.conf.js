688e33c5f3f3dffa800d799fec94e13493e8c0b5
Florian Markert <florian@scienceflow.de>
2014-12-05 12:13:17 +0100

    coverage added

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,7 @@ module.exports = function (config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
- //     'src/*.js': ['coverage'],
+      'src/*.js': ['coverage']
 //      'src/**/*.js': ['coverage']
     },
 
