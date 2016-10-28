7234543f2498e89ae267af315e6e2f0d4bd19547
Florian Markert <florian@scienceflow.de>
2016-10-28 16:14:33 +0200

    update to version 2.0.0

--- dist/fili.js
+++ dist/fili.js
@@ -1,6 +1,6 @@
 /**
  * @name    fili
- * @version 1.6.0 | October 28th 2016
+ * @version 2.0.0 | October 28th 2016
  * @author  Florian Markert
  * @license MIT
  */
@@ -309,7 +309,7 @@ var Fft = function Fft(radix) {
       correction: 4 / 3
     },
     blackmanHarris: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.35875 - 0.48829 * cos(z) + 0.14128 * cos(2 * z) - 0.01168 * cos(3 * z);
       },
@@ -317,7 +317,7 @@ var Fft = function Fft(radix) {
       correction: 1.5594508635
     },
     nuttall3: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.375 - 0.5 * cos(z) + 0.125 * cos(2 * z);
       },
@@ -325,7 +325,7 @@ var Fft = function Fft(radix) {
       correction: 1.56
     },
     nuttall3a: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.40897 - 0.5 * cos(z) + 0.09103 * cos(2 * z);
       },
@@ -333,7 +333,7 @@ var Fft = function Fft(radix) {
       correction: 1.692
     },
     nuttall3b: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.4243801 - 0.4973406 * cos(z) + 0.078793 * cos(2 * z);
       },
@@ -341,7 +341,7 @@ var Fft = function Fft(radix) {
       correction: 1.7372527
     },
     nuttall4: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.3125 - 0.46875 * cos(z) + 0.1875 * cos(2 * z) - 0.03125 * cos(3 * z);
       },
@@ -349,7 +349,7 @@ var Fft = function Fft(radix) {
       correction: 1.454543
     },
     nuttall4a: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.338946 - 0.481973 * cos(z) + 0.161054 * cos(2 * z) - 0.018027 * cos(3 * z);
       },
@@ -357,7 +357,7 @@ var Fft = function Fft(radix) {
       correction: 1.512732763
     },
     nuttall4b: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.355768 - 0.481973 * cos(z) + 0.144232 * cos(2 * z) - 0.012604 * cos(3 * z);
       },
@@ -365,7 +365,7 @@ var Fft = function Fft(radix) {
       correction: 1.55223262
     },
     nuttall4c: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.3635819 - 0.4891775 * cos(z) + 0.1365995 * cos(2 * z) - 0.0106411 * cos(3 * z);
       },
@@ -374,7 +374,7 @@ var Fft = function Fft(radix) {
     },
     // fast decaying flat top
     sft3f: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.26526 - 0.5 * cos(z) + 0.23474 * cos(2 * z);
       },
@@ -382,7 +382,7 @@ var Fft = function Fft(radix) {
       correction: 1.3610238
     },
     sft4f: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.21706 - 0.42103 * cos(z) + 0.28294 * cos(2 * z) - 0.07897 * cos(3 * z);
       },
@@ -390,7 +390,7 @@ var Fft = function Fft(radix) {
       correction: 1.2773573
     },
     sft5f: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.1881 - 0.36923 * cos(z) + 0.28702 * cos(2 * z) - 0.13077 * cos(3 * z) + 0.02488 * cos(4 * z);
       },
@@ -399,7 +399,7 @@ var Fft = function Fft(radix) {
     },
     // minimum sidelobe flat top
     sft3m: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.28235 - 0.52105 * cos(z) + 0.19659 * cos(2 * z);
       },
@@ -407,7 +407,7 @@ var Fft = function Fft(radix) {
       correction: 1.39343451
     },
     sft4m: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.241906 - 0.460841 * cos(z) + 0.2552381 * cos(2 * z) - 0.041872 * cos(3 * z);
       },
@@ -415,7 +415,7 @@ var Fft = function Fft(radix) {
       correction: 1.3190596
     },
     sft5m: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.209671 - 0.407331 * cos(z) + 0.281225 * cos(2 * z) - 0.092669 * cos(3 * z) + 0.0091036 * cos(4 * z);
       },
@@ -423,7 +423,7 @@ var Fft = function Fft(radix) {
       correction: 1.26529456464
     },
     nift: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return 0.2810639 - 0.5208972 * cos(z) + 0.1980399 * cos(2 * z);
       },
@@ -431,7 +431,7 @@ var Fft = function Fft(radix) {
       correction: 1.39094182
     },
     hpft: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.912510941 * cos(z) + 1.079173272 * cos(2 * z) - 0.1832630879 * cos(3 * z)) / N;
       },
@@ -439,7 +439,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     srft: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.93 * cos(z) + 1.29 * cos(2 * z) - 0.388 * cos(3 * z) + 0.028 * cos(4 * z)) / N;
       },
@@ -447,14 +447,15 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft70: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.90796 * cos(z) + 1.07349 * cos(2 * z) - 0.18199 * cos(3 * z)) / N;
       },
+      values: [],
       correction: 1
     },
     hft95: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.9383379 * cos(z) + 1.3045202 * cos(2 * z) - 0.402827 * cos(3 * z) + 0.0350665 * cos(4 * z)) / N;
       },
@@ -462,7 +463,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft90d: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.942604 * cos(z) + 1.340318 * cos(2 * z) - 0.440811 * cos(3 * z) + 0.043097 * cos(4 * z)) / N;
       },
@@ -470,7 +471,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft116d: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.9575375 * cos(z) + 1.4780705 * cos(2 * z) - 0.6367431 * cos(3 * z) + 0.1228389 * cos(4 * z) - 0.0066288 * cos(5 * z)) / N;
       },
@@ -478,7 +479,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft144d: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.96760033 * cos(z) + 1.57983607 * cos(2 * z) - 0.81123644 * cos(3 * z) + 0.22583558 * cos(4 * z) - 0.02773848 * cos(5 * z) + 0.0009036 * cos(6 * z)) / N;
       },
@@ -486,7 +487,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft196d: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.97441842 * cos(z) + 1.65409888 * cos(2 * z) - 0.95788186 * cos(3 * z) + 0.3367342 * cos(4 * z) - 0.06364621 * cos(5 * z) + 0.00521942 * cos(6 * z) - 0.00010599 * cos(7 * z)) / N;
       },
@@ -494,7 +495,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft223d: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.98298997309 * cos(z) + 1.75556083063 * cos(2 * z) - 1.19037717712 * cos(3 * z) + 0.56155440797 * cos(4 * z) - 0.17296769663 * cos(5 * z) + 0.03233247087 * cos(6 * z) - 0.00324954578 * cos(7 * z) + 0.00013801040 * cos(8 * z) - 0.00000132725 * cos(9 * z)) / N;
       },
@@ -502,7 +503,7 @@ var Fft = function Fft(radix) {
       correction: 1
     },
     hft248d: {
-      calc: function calc(n, N, a) {
+      calc: function calc(n, N) {
         var z = PI2 * n / (N - 1);
         return (1.0 - 1.985844164102 * cos(z) + 1.791176438506 * cos(2 * z) - 1.282075284005 * cos(3 * z) + 0.667777530266 * cos(4 * z) - 0.240160796576 * cos(5 * z) + 0.056656381764 * cos(6 * z) - 0.008134974479 * cos(7 * z) + 0.00062454465 * cos(8 * z) - 0.000019808998 * cos(9 * z) + 0.000000132974 * cos(10 * z)) / N;
       },
